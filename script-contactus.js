9e063819d92a63c54533760a60ec9a6368f7fc8a
Bruno Cason <bruno.cason.pb@compasso.com.br>
2024-04-19 14:40:18 -0300

    Tela de erro - Pagina não encontrada

--- script-contactus.js
+++ script-contactus.js
@@ -1,12 +1,14 @@
+// Formulario
+
 // Regex Nomes e email
 const validarEmail = /\w+@\w+\.\w+/;    // regex para validar email
-const validarNome = /^[a-zA-Z]{3,15}$/
+const validarNome = /^[a-zA-Z]{3,15}$/  // regex para validar nomes
 
 // Mensagens de erro
-let msgErroEmailForm = document.getElementById('msg-erro-email-form')
-let msgErroMessageForm = document.getElementById('msg-erro-message-form')
-let msgErroFirstNameForm = document.getElementById('msg-erro-firstname')
-let msgErroLastNameForm = document.getElementById('msg-erro-lastname')
+let msgErroFirstNameForm = document.getElementById('msg-erro-firstname')    // first name
+let msgErroLastNameForm = document.getElementById('msg-erro-lastname')      // last name
+let msgErroEmailForm = document.getElementById('msg-erro-email-form')       // email
+let msgErroMessageForm = document.getElementById('msg-erro-message-form')   // message
 
 // Validação First Name
 function validarFirstName() {
@@ -14,12 +16,12 @@ function validarFirstName() {
     let firstName = firstNameForm.value
 
     if(validarNome.test(firstName)) {
-        firstNameForm.style.border = '2px solid Green';
-        msgErroFirstNameForm.style.display = 'none'
+        firstNameForm.style.border = '2px solid Green';     // se seguir o padrão do regex
+        msgErroFirstNameForm.style.display = 'none'         // borda fica verde
     }
     else {
-        firstNameForm.style.border = '2px solid Red'; 
-        msgErroFirstNameForm.style.display = 'block'
+        firstNameForm.style.border = '2px solid Red';       // se não seguir o padrão do regex
+        msgErroFirstNameForm.style.display = 'block'        // borda fica vermelha e retorna erro
     }
 }
 
@@ -29,12 +31,12 @@ function validarLastName() {
     let lastName = lastNameForm.value
 
     if(validarNome.test(lastName)) {
-        lastNameForm.style.border = '2px solid Green';
-        msgErroLastNameForm.style.display = 'none'
-    }
+        lastNameForm.style.border = '2px solid Green';     // se seguir o padrão do regex
+        msgErroLastNameForm.style.display = 'none'         // borda fica verde
+    }   
     else {
-        lastNameForm.style.border = '2px solid Red'; 
-        msgErroLastNameForm.style.display = 'block'
+        lastNameForm.style.border = '2px solid Red';       // se não seguir o padrão do regex
+        msgErroLastNameForm.style.display = 'block'        // borda fica vermelha e retorna erro
     }
 }
 
@@ -44,15 +46,14 @@ function ValidarEmailForm() {
     let email = emailForm.value
 
     if(validarEmail.test(email)) {
-        emailForm.style.border = '2px solid Green';    // caso o email esteja correto, borda do input fica verde
-        msgErroEmailForm.style.display = 'none'
+        emailForm.style.border = '2px solid Green';    // caso o email esteja correto
+        msgErroEmailForm.style.display = 'none'        //borda do input fica verde
     }
     else {
-        emailForm.style.border = '2px solid Red'; 
-        msgErroEmailForm.style.display = 'block'
+        emailForm.style.border = '2px solid Red';      // caso o email esteja incorreto
+        msgErroEmailForm.style.display = 'block'       //borda fica vermelhor e retorna erro
     }
 }
-
 
 // Validação Message
 function ValidarMessageForm() {
@@ -60,17 +61,16 @@ function ValidarMessageForm() {
     let message = messageForm.value
 
     if(message.length < 3) {
-        messageForm.style.border = '2px solid Red'; 
-        msgErroMessageForm.style.display = 'block'
+        messageForm.style.border = '2px solid Red';     // se a mensagem for menor que 3 caracteres
+        msgErroMessageForm.style.display = 'block'      // borda fica vermelha e retorna erro
     }
     else {
-        messageForm.style.border = '2px solid Green';    // caso o email esteja correto, borda do input fica verde
-        msgErroMessageForm.style.display = 'none'
+        messageForm.style.border = '2px solid Green';   // se a mensagem foi maior que 3 caracteres
+        msgErroMessageForm.style.display = 'none'       // borda fica verde
     }
 }
 
-
-// Validação do email
+// Validação do email Subscribe
 let msgErroEmail = document.getElementById('mensagem-erro-email')
 
 function validar() {
@@ -78,10 +78,10 @@ function validar() {
     let email = emailInput.value;
 
     if (validarEmail.test(email)) {
-        emailInput.style.border = '2px solid Green';    // caso o email esteja correto, borda do input fica verde
-        msgErroEmail.style.display = 'none'
+        emailInput.style.border = '2px solid Green';    // caso o email esteja correto
+        msgErroEmail.style.display = 'none'             // borda do input fica verde
     } else {
-        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto, borda do input fica vermelho
-        msgErroEmail.style.display = 'block'                 // e aparece a mensagem de erro
+        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto
+        msgErroEmail.style.display = 'block'            // borda do fica vermelhor e retorna erro
     }
 }
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Validação do email
+// Validação do email Subscribe
 
 const validarEmail = /\w+@\w+\.\w+/;    // regex para validar email
-let msgErro = document.getElementById('mensagem-erro')
+let msgErro = document.getElementById('mensagem-erro') // Mensagen de erro
 
 function validar() {
     let emailInput = document.getElementById('input-email');
     let email = emailInput.value;
 
     if (validarEmail.test(email)) {
-        emailInput.style.border = '2px solid Green';    // caso o email esteja correto, borda do input fica verde
-        msgErro.style.display = 'none'
+        emailInput.style.border = '2px solid Green';    // caso o email esteja correto
+        msgErro.style.display = 'none'                  // borda do input fica verde
         
     } else {
-        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto, borda do input fica vermelho
-        msgErro.style.display = 'block'                 // e aparece a mensagem de erro
+        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto
+        msgErro.style.display = 'block'                 // borda do fica vermelhor e retorna erro
     }
 }
