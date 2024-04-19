80baed2618476134ab26c090f4840d7f1d3b9fff
Bruno Cason <bruno.cason.pb@compasso.com.br>
2024-04-19 17:22:51 -0300

    Armazenando dados no LocalStorage

--- script-contactus.js
+++ script-contactus.js
@@ -1,62 +1,58 @@
-// Formulario
+function armazenarForm() {
+    // Regex Nomes e email
+    const validarEmail = /\w+@\w+\.\w+/;    // regex para validar email
+    const validarNome = /^[a-zA-Z]{3,15}$/  // regex para validar nomes
 
-// Regex Nomes e email
-const validarEmail = /\w+@\w+\.\w+/;    // regex para validar email
-const validarNome = /^[a-zA-Z]{3,15}$/  // regex para validar nomes
+    // Mensagens de erro
+    let msgErroFirstNameForm = document.getElementById('msg-erro-firstname')    // first name
+    let msgErroLastNameForm = document.getElementById('msg-erro-lastname')      // last name
+    let msgErroEmailForm = document.getElementById('msg-erro-email-form')       // email
+    let msgErroMessageForm = document.getElementById('msg-erro-message-form')   // message
 
-// Mensagens de erro
-let msgErroFirstNameForm = document.getElementById('msg-erro-firstname')    // first name
-let msgErroLastNameForm = document.getElementById('msg-erro-lastname')      // last name
-let msgErroEmailForm = document.getElementById('msg-erro-email-form')       // email
-let msgErroMessageForm = document.getElementById('msg-erro-message-form')   // message
-
-// Validação First Name
-function validarFirstName() {
+    // Validação do First Name
     let firstNameForm = document.getElementById('input-first-name')
     let firstName = firstNameForm.value
 
     if(validarNome.test(firstName)) {
         firstNameForm.style.border = '2px solid Green';     // se seguir o padrão do regex
         msgErroFirstNameForm.style.display = 'none'         // borda fica verde
+        localStorage.setItem('firstName', firstName);
     }
     else {
         firstNameForm.style.border = '2px solid Red';       // se não seguir o padrão do regex
         msgErroFirstNameForm.style.display = 'block'        // borda fica vermelha e retorna erro
     }
-}
 
-// Validação Last Name
-function validarLastName() {
+    // Validação Last Name
     let lastNameForm = document.getElementById('input-last-name')
     let lastName = lastNameForm.value
 
     if(validarNome.test(lastName)) {
         lastNameForm.style.border = '2px solid Green';     // se seguir o padrão do regex
         msgErroLastNameForm.style.display = 'none'         // borda fica verde
+        localStorage.setItem('lastName', lastName);
     }   
     else {
         lastNameForm.style.border = '2px solid Red';       // se não seguir o padrão do regex
         msgErroLastNameForm.style.display = 'block'        // borda fica vermelha e retorna erro
     }
-}
 
-// Validação do email form
-function ValidarEmailForm() {
+    //Validação do email form
     let emailForm = document.getElementById('input-email-form')
     let email = emailForm.value
 
     if(validarEmail.test(email)) {
         emailForm.style.border = '2px solid Green';    // caso o email esteja correto
         msgErroEmailForm.style.display = 'none'        //borda do input fica verde
+        localStorage.setItem('email', email);
     }
     else {
         emailForm.style.border = '2px solid Red';      // caso o email esteja incorreto
         msgErroEmailForm.style.display = 'block'       //borda fica vermelhor e retorna erro
     }
-}
 
-// Validação Message
-function ValidarMessageForm() {
+    // Validação Message
+
     let messageForm = document.getElementById('input-message-form')
     let message = messageForm.value
 
@@ -67,21 +63,27 @@ function ValidarMessageForm() {
     else {
         messageForm.style.border = '2px solid Green';   // se a mensagem foi maior que 3 caracteres
         msgErroMessageForm.style.display = 'none'       // borda fica verde
+        localStorage.setItem('message', message);
     }
 }
 
-// Validação do email Subscribe
-let msgErroEmail = document.getElementById('mensagem-erro-email')
+function armazenarEmailSub() {
 
-function validar() {
+    // Validação email
+    const validarEmail = /\w+@\w+\.\w+/;                        // regex para validar email
+    let msgErroEmail = document.getElementById('mensagem-erro-email')      // Mensagen de erro
     let emailInput = document.getElementById('input-email');
     let email = emailInput.value;
 
     if (validarEmail.test(email)) {
-        emailInput.style.border = '2px solid Green';    // caso o email esteja correto
-        msgErroEmail.style.display = 'none'             // borda do input fica verde
+        emailInput.style.border = '2px solid Green';            // caso o email esteja correto
+        msgErroEmail.style.display = 'none'                          // borda do input fica verde
+
+        // Armazena o email no localstorage caso ele seja validado
+        localStorage.setItem('email', email);
+            
     } else {
-        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto
-        msgErroEmail.style.display = 'block'            // borda do fica vermelhor e retorna erro
+        emailInput.style.border = '2px solid Red';              // caso o email esteja incorreto
+        msgErroEmail.style.display = 'block'                         // borda do fica vermelhor e retorna erro
     }
 }
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// Validação do email Subscribe
+// Função para armazenar no localstorage
+function armazenarEmail() {
 
-const validarEmail = /\w+@\w+\.\w+/;    // regex para validar email
-let msgErro = document.getElementById('mensagem-erro') // Mensagen de erro
-
-function validar() {
+    // Validação email
+    const validarEmail = /\w+@\w+\.\w+/;                        // regex para validar email
+    let msgErro = document.getElementById('mensagem-erro')      // Mensagen de erro
     let emailInput = document.getElementById('input-email');
     let email = emailInput.value;
 
     if (validarEmail.test(email)) {
-        emailInput.style.border = '2px solid Green';    // caso o email esteja correto
-        msgErro.style.display = 'none'                  // borda do input fica verde
-        
+        emailInput.style.border = '2px solid Green';            // caso o email esteja correto
+        msgErro.style.display = 'none'                          // borda do input fica verde
+
+        // Armazena o email no localstorage caso ele seja validado
+        localStorage.setItem('email', email);
+            
     } else {
-        emailInput.style.border = '2px solid Red';      // caso o email esteja incorreto
-        msgErro.style.display = 'block'                 // borda do fica vermelhor e retorna erro
+        emailInput.style.border = '2px solid Red';              // caso o email esteja incorreto
+        msgErro.style.display = 'block'                         // borda do fica vermelhor e retorna erro
     }
 }
